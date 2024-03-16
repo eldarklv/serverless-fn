911b358efdbdf27a4608041062529badc22de4c9
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-03-17 00:14:39 +0300

    do serverless api functions

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+app.use(express.json());
+
+const characters = [
+  {
+    id: 1,
+    name: "Имя персонажа",
+    description: "описание...",
+    modified: "2020-07-21",
+    thumbnail: "http://...",
+    comics: [
+      {
+        id: 1,
+        name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Имя персонажа 2",
+    description: "описание...",
+    modified: "2020-07-21",
+    thumbnail: "http://...",
+    comics: [
+        {
+            id: 1,
+            name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1"
+        }
+    ]
+}
+];
+
+app.get("/api/characters", (req, res) => {
+  res.json(characters);
+});
+
+app.get("/api/characters/:id", (req, res) => {
+  const character = characters.find((c) => c.id === +req.params.id);
+  res.json(character);
+};
+
+module.exports.handler = serverless(app);
