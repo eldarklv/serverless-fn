72ed54b3ab4bc523504ad4e9f7da7fcff14988a2
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2024-03-17 00:15:19 +0300

    fix bracets

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ const characters = [
     modified: "2020-07-21",
     thumbnail: "http://...",
     comics: [
-        {
-            id: 1,
-            name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1"
-        }
-    ]
-}
+      {
+        id: 1,
+        name: "Spider-Man: 101 Ways to End the Clone Saga (1997) #1",
+      },
+    ],
+  },
 ];
 
 app.get("/api/characters", (req, res) => {
@@ -41,6 +41,6 @@ app.get("/api/characters", (req, res) => {
 app.get("/api/characters/:id", (req, res) => {
   const character = characters.find((c) => c.id === +req.params.id);
   res.json(character);
-};
+});
 
 module.exports.handler = serverless(app);
